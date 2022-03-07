be6a0bbe62546fbeec0379018ad3830ac611864a
mazari <luigi.mazari@gmail.com>
2022-03-07 12:18:54 +0100

    modified loglevel

--- edm264_logger_to_db/logger.py
+++ edm264_logger_to_db/logger.py
@@ -6,7 +6,7 @@ def getgglogger(name):
     console_formatter = logging.Formatter('%(levelname)s -- %(message)s')
 
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(console_formatter)
 
     logger = logging.getLogger(name)
